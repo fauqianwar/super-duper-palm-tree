23e03866f466a3212b2e042a7c4ed9804db3a115
fauqianwar <122507998+fauqianwar@users.noreply.github.com>
2023-01-16 13:16:06 +0700

    Create index-2.php
    games review blog

---
+++ index-2.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<html>
+    <head>
+        <title>game review</title>
+        <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="header">
+    <?php
+    echo "<h1>mobile legends review</h1>";
+    ?>
+    <div class="input-form">
+    <input placeholder="username">
+    <input placeholder="password" type="password">
+    <button>Sign in</buttton>
+</div>
+</div>
+    <div class="content">
+    <div class="introduction item">
+        <h2>game preview</h2>
+        <p>Mobile Legends: Bang Bang is a popular mobile multiplayer online battle arena (MOBA) 
+            game developed and published by Moonton. It is free-to-play on both iOS and Android platforms.
+             Players compete in 5v5 battles, controlling a hero with unique abilities to defeat the opposing team 
+             and destroy their base. The game features a variety of game modes, a large roster of playable heroes, and regular updates and events. 
+             It has become one of the most popular mobile games in the world, with a huge player base and competitive scene
+        </p>  
+</div>
+<div class="how-to-play item">
+<h2>how to play</h2>
+Mobile Legends: Bang Bang is a 5v5 multiplayer online battle arena (MOBA) game where players control a hero with unique abilities to defeat the opposing team and destroy their base. Here's a shorter guide on how to play the game:
+1.	Select a hero: Choose a hero from the roster of playable characters, each with their own unique abilities and playstyle.
+2.	Lane assignment: Players are assigned to one of the three lanes (top, middle, and bottom) or to the jungle area
+3.	Farming: Earn gold and experience by farming minions and monsters in the lanes and jungle.
+4.	Team fighting: Work with your team to push down lanes, gank enemy heroes, and destroy the enemy base.
+5.	Objective control: Control the objectives in the map like the Lord and Turtle.
+6.	Map awareness: Always be aware of the minimap and positions of your team and the enemy team.
+7.	Communication: Communicate with your team to coordinate strategies and make plays together.
+8.	Itemization: Consider what items to buy for the hero to increase strength and abilities.  
+</div>
+<div class="attraction item">
+<h2>attraction</h2>
+Mobile Legends: Bang Bang is a highly attractive game to play for several reasons:
+1.	Easy to learn, hard to master: The game is relatively easy to pick up, with simple controls and a straightforward objective. However, mastering the game requires a lot of skill, strategy, and teamwork, making it a challenging and rewarding experience for players.
+2.	Constant updates and events: The game regularly releases updates and events that bring new heroes, skins, modes, and features to the game. This keeps the game fresh and interesting for players, and also provides opportunities for players to acquire new heroes, skins and etc.
+3.	Competitive scene: The game has a strong competitive scene, with regular tournaments and leagues taking place at both the local and international level. This provides players with the opportunity to test their skills against other skilled players and compete for prizes.
+4.	Large roster of heroes: The game features a large roster of playable heroes, each with their own unique abilities and playstyles. This provides players with a lot of variety and allows them to find a hero that suits their playstyle.
+5.	Social aspect: The game is also social, players can play with friends, join a guild or a community, and interact with other players in the game.
+6.	Available on mobile: Mobile Legends is available on mobile, making it accessible to players anywhere and at any time.
+7.	Free-to-play: The game is free-to-play, which means that players can download and play the game without paying anything.
+All of these factors combined make Mobile Legends: Bang Bang an attractive game to play for players of all skill levels and interests.
+</div>  
+</body>
+</html>
